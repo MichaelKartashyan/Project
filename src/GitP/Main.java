860118ea0862d7df8a53ef83034723418fa63fff
MichaelKartashyan <mike.kartashyan@mail.ru>
2021-04-30 18:02:40 +0600

    Initial commit

--- src/GitP/Main.java
+++ src/GitP/Main.java
@@ -1,7 +1,12 @@
 package GitP;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        System.out.println(i*i);
     }
 }
